30792c713c3faf21ca78b2d62b5b463a3892086b
Samuel Battams-Scott <psysb8@nottingham.ac.uk>
2021-07-05 01:00:40 +0100

    first commit

---
+++ helloWorld.hs
@@ -0,0 +1,17 @@
+import Graphics.UI.GLUT
+import Data.IORef
+import Bindings
+
+main :: IO ()
+main = do
+  (_progName, _args) <- getArgsAndInitialize
+  initialDisplayMode $= [DoubleBuffered]
+  _window <- createWindow "Hello World"
+  reshapeCallback $= Just reshape
+  angle <- newIORef 0
+  delta <- newIORef 0.1
+  pos <- newIORef (0, 0)
+  keyboardMouseCallback $= Just (keyboardMouse delta pos)
+  idleCallback $= Just (idle angle delta)
+  displayCallback $= display angle pos
+  mainLoop
